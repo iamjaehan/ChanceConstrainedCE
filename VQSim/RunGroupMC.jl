86695b25195de40d5145a1ff56fe9b5f3c553d14
jaehan im <jaehan.im@utexas.edu>
2026-02-09 23:42:23 -0600

    7 base test

--- VQSim/RunGroupMC.jl
+++ VQSim/RunGroupMC.jl
@@ -5,7 +5,7 @@ cfgE = MCEpochConfig(
     csv_path = "schedule/flight_schedule_1h_5b.csv",
     params = VQState.SimParams(2, [2,2]),
     max_subset_size = 1024,
-    B0_total = 6,
+    B0_total = 7,
     Q0_runway = [3, 4],
     t_epoch = 0,
     lateness_mean = 0.0,
@@ -27,7 +27,7 @@ cfgE = MCEpochConfig(
 )
 
 println("Running test case 1")
-df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_6a_0s_90c.csv")
+df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_7a_0s_90c.csv")
 
 ### Test case 2 ### 5 / 0 / 0.9
 cfgE.B0_total = 5
@@ -40,9 +40,9 @@ println("Running test case 3")
 df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_4a_0s_90c.csv")
 
 ### Test case 4 ### 7 / 0 / 0.9
-cfgE.B0_total = 7
+cfgE.B0_total = 6
 println("Running test case 4")
-df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_7a_0s_90c.csv")
+df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_6a_0s_90c.csv")
 
 ## Test case 5 ### 8 / 0 / 0.9
 cfgE.B0_total = 8
@@ -50,47 +50,47 @@ println("Running test case 5")
 df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_8a_0s_90c.csv")
 
 ### Test case 6 ### 6 / 5 / 0.9
-cfgE.B0_total = 6
+cfgE.B0_total = 7
 cfgE.coord_sigma_scalar = 5
 cfgE.real_sigma_scalar = 5
 println("Running test case 6")
-df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_6a_5s_90c.csv")
+df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_7a_5s_90c.csv")
 
 ### Test case 7 ### 6 / 15 / 0.9
 cfgE.coord_sigma_scalar = 20
 cfgE.real_sigma_scalar = 20
 println("Running test case 7")
-df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_6a_20s_90c.csv")
+df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_7a_20s_90c.csv")
 
 ### Test case 8 ### 6 / 30 / 0.9
 cfgE.coord_sigma_scalar = 45
 cfgE.real_sigma_scalar = 45
 println("Running test case 8")
-df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_6a_45s_90c.csv")
+df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_7a_45s_90c.csv")
 
 ### Test case 9 ### 6 / 15 / 0.5
 cfgE.coord_sigma_scalar = 20
 cfgE.real_sigma_scalar = 20
 cfgE.alpha = 0.5
 println("Running test case 9")
-df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_6a_20s_50c.csv")
+df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_7a_20s_50c.csv")
 
 ### Test case 10 ### 6 / 15 / 0.75
 cfgE.alpha = 0.75
 println("Running test case 10")
-df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_6a_20s_75c.csv")
+df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_7a_20s_75c.csv")
 
 ### Test case 11 ### 6 / 15 / 0.95
 cfgE.alpha = 0.95
 println("Running test case 11")
-df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_6a_20s_95c.csv")
+df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_7a_20s_95c.csv")
 
 ### Test case 12 ### 6 / 15 / 0.99
 cfgE.alpha = 0.99
 println("Running test case 12")
-df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_6a_20s_99c.csv")
+df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_7a_20s_99c.csv")
 
 ### Test case 13 ### 6 / 15 / 0.99
 cfgE.alpha = 0.3
 println("Running test case 13")
-df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_6a_20s_30c.csv")
+df = run_mc_epoch_test(cfgE; out_csv="mc_epoch_results_7a_20s_30c.csv")
